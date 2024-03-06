c76f788b3a7368f86b5e6895e83a88231bccbaef
Eskil Brännerud <eskbr129@student.liu.se>
2024-03-06 13:36:59 +0100

    now can change page

--- tailwindtest/app/sidebar.tsx
+++ tailwindtest/app/sidebar.tsx
@@ -1,6 +1,7 @@
 
 import { UilSchedule, UilInfoCircle } from '@iconscout/react-unicons'
 import Image from 'next/image'
+import Link from 'next/link';
 export default function Sidebar() {
   return (
     <div className="bg-slate-100 divide-y divide-gray-400 h-screen flex flex-col" >
@@ -13,9 +14,9 @@ export default function Sidebar() {
       <div className='p-5'>
         <h2 className="text-gray-800">Menu</h2>
         <ul>
-          <button className="text-gray-500 font-bold text-2xl flex"> <UilSchedule  color="#6b7280" /> Meetings</button>
-          <button className="text-gray-500 font-bold text-2xl flex"> <UilSchedule  color="#6b7280" /> Document</button>
-          <button className="text-gray-500 font-bold text-2xl flex"> <UilSchedule  color="#6b7280" /> Templates</button>
+          <Link href="/meetings" className="text-gray-500 font-bold text-2xl flex"> <UilSchedule  color="#6b7280" /> Meetings</Link>
+          <Link href="/documents" className="text-gray-500 font-bold text-2xl flex"> <UilSchedule  color="#6b7280" /> Document</Link>
+          <Link href="/templates" className="text-gray-500 font-bold text-2xl flex"> <UilSchedule  color="#6b7280" /> Templates</Link>
         </ul>
       </div>
       <div className='bg-fuchsia-900 flex flex-grow'>
